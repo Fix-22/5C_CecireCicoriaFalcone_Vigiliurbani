49be1c62d45e347b066f75eefa0aaaea609eddc1
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-11-12 17:11:19 +0100

    aggiunto componente per barra di ricerca

--- searchbarComponent/searchbarComponent.js
+++ searchbarComponent/searchbarComponent.js
@@ -1,3 +1,23 @@
 export const generateSearchbar = (parentElement) => {
+    let placeholder;
+    let callback;
 
+    return {
+        build: (inputPlaceholder) => {
+            placeholder = inputPlaceholder;
+        },
+        onsearch: (inputCallback) => {
+            callback = inputCallback;
+        },
+        render: () => {
+            let HTML = '<div class="input-group mb-3"><input type="text" class="form-control" placeholder="' + placeholder + '" id="searchText"><button type="button" class="btn btn-primary" id="searchButton"><i class="bi bi-search"></i></button></div>';
+
+            parentElement.innerHTML = HTML;
+            
+            document.getElementById("searchButton").onclick = () => {
+                let searchText = document.getElementById("searchText").value;
+                callback(searchText);
+            };
+        }
+    };
 };
