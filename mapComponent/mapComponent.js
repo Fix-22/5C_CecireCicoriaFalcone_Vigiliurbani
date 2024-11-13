cc5ce6f0f5677683955290b4725d2357ab9105a2
Simone Cecire <ceciresimone@itis-molinari.eu>
2024-11-13 16:02:38 +0100

    ripristinato funzionamento componente mappa

--- mapComponent/mapComponent.js
+++ mapComponent/mapComponent.js
@@ -8,17 +8,15 @@ export const generateMap = (parentElement) => {
             L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 maxZoom: 19,
                 attribution: '© <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-             }).addTo(map);
+            }).addTo(map);
         },
         render: () => {
             accidents.forEach((accident) => {
-                const marker = L.marker(accident.coords).addTo(map);
-                marker.bindPopup("<b>" + accident.address + "</b><br><b>Data:</b> " + accident.dateTime + "<br><b>Targhe coinvolte:</b> " + accident.plates.join(", ") + "<br><b>Feriti:</b> " + accident.injured + "<br><b>Morti:</b> " + accident.deaths);
-             });
-            
-             if (accidents.length > 0) {
-                map.setView(accidents[accidents.length - 1].coords, 14);
-             }
+                if (accident) {
+                    const marker = L.marker(accident.coords).addTo(map);
+                    marker.bindPopup("<b>" + accident.address + "</b><br><b>Data:</b> " + accident.dateTime + "<br><b>Targhe coinvolte:</b> " + accident.plates.join(", ") + "<br><b>Feriti:</b> " + accident.injured + "<br><b>Morti:</b> " + accident.deaths);
+                }
+            });
         },
         addAccident: (accident) => {
             if (accidents.indexOf(accident) === -1) {
