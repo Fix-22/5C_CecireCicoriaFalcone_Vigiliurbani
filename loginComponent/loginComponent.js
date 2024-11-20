21f39e15c16608a4aadda596e134df20db575c01
DavideCicoria <cicoriadavide@itis-molinari.eu>
2024-11-20 10:50:31 +0100

    register

--- loginComponent/loginComponent.js
+++ loginComponent/loginComponent.js
@@ -22,6 +22,25 @@ export const generateLoginComponent = (parentElement) => {
         });
     };
 
+    const register = (username, password) => {
+        return new Promise((resolve, reject) => {
+            fetch("https://ws.cipiaceinfo.it/credential/register", {
+                method: "POST",
+                headers: {
+                    "content-type": "application/json",
+                    "key": token
+                },
+                body: JSON.stringify({
+                    username: username,
+                    password: password
+                })
+            })
+            .then(r => r.json())
+            .then(data => resolve(data.result))
+            .catch(err => reject(err.result));
+        });
+    };
+
     return {
         build: (inputToken, inputPrivateClass) => {
             token = inputToken;
@@ -44,9 +63,11 @@ export const generateLoginComponent = (parentElement) => {
                                 <div class="col">
                                     <input type="password" id="passwordInput" class="form-control sm" placeholder="Password">
                                 </div>
-                                <div class="col">
+                                <div class="col-4">
                                     <button type="button" id="loginButton" class="btn btn-primary mb-2">Accedi</button>
+                                    <button type="button" id="registerButton" class="btn btn-primary mb-2">Registrati</button>
                                 </div>
+                                <div id="loginResultLabel"></div>
                             </div>
                         </form>`;
             parentElement.innerHTML = html;
@@ -58,6 +79,7 @@ export const generateLoginComponent = (parentElement) => {
                 if (username && password) {
                     login(username, password)
                     .then(r => {
+                        let loginResultLabel = document.getElementById("loginResultLabel") ;
                         if (r) {
                             sessionStorage.setItem("logged", true);
                             isLogged = true;
@@ -68,7 +90,60 @@ export const generateLoginComponent = (parentElement) => {
                                 e.classList.remove("d-none");
                             });
 
+                            loginResultLabel.classList.add("text-success") ;
+                            loginResultLabel.classList.remove("text-danger") ;
+                            loginResultLabel.innerText = "Login effettuato con successo" ;
+
+                            setTimeout(() => {
+                                document.getElementById("loginContainer").classList.add("d-none");
+                            }, 1500) ;
+
+                        } else {
+                            loginResultLabel.classList.add("text-danger") ;
+                            loginResultLabel.classList.remove("text-success") ;
+                            loginResultLabel.innerText = "Nome utente o password errati!" ;
+                        }
+                    })
+                    .catch(err => {
+
+                    });
+                }
+            };
+
+            document.getElementById("registerButton").onclick = () => {
+                const username = document.getElementById("usernameInput").value;
+                const password = document.getElementById("passwordInput").value;
+
+                if (username && password) {
+                    register(username, password)
+                    .then(r => {
+                        if (r) {
+                            console.log("registrato e loggato") ;
+                            sessionStorage.setItem("logged", true);
+                            isLogged = true;
+                            document.getElementById("usernameInput").value = "";
+                            document.getElementById("passwordInput").value = "";
+
+                            document.querySelectorAll("." + privateClass).forEach(e => {
+                                e.classList.remove("d-none");
+                            });
+
                             document.getElementById("loginContainer").classList.add("d-none");
+                        
+
+                            //modificare per register
+                            loginResultLabel.classList.add("text-success") ;
+                            loginResultLabel.classList.remove("text-danger") ;
+                            loginResultLabel.innerText = "Login effettuato con successo" ;
+
+                            setTimeout(() => {
+                                document.getElementById("loginContainer").classList.add("d-none");
+                            }, 1500) ;
+
+                        } else {
+                            loginResultLabel.classList.add("text-danger") ;
+                            loginResultLabel.classList.remove("text-success") ;
+                            loginResultLabel.innerText = "Nome utente o password errati!" ;
                         }
                     })
                     .catch(err => {
